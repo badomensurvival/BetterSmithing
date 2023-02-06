21bd85c0fb99da6582989053712aa7b419e2d76d
Bruno de Araujo Alves <me@baraus.dev>
2023-02-06 10:13:03 -0300

    Update

--- src/main/java/dev/baraus/bettersmithing/BetterSmithing.kt
+++ src/main/java/dev/baraus/bettersmithing/BetterSmithing.kt
@@ -16,6 +16,10 @@ class BetterSmithing : JavaPlugin(), Listener {
         "SWORD", "SHOVEL", "PICKAXE", "AXE", "HOE", "HELMET", "CHESTPLATE", "LEGGINGS", "BOOTS"
     )
 
+    var leatherTools = Material.values().filter {
+        it.name.startsWith("LEATHER_") && it.name.removePrefix("LEATHER_") in toolTypes
+    }
+
     var woodenTools = Material.values().filter {
         it.name.startsWith("WOODEN_") && it.name.removePrefix("WOODEN_") in toolTypes
     }
@@ -48,7 +52,7 @@ class BetterSmithing : JavaPlugin(), Listener {
                 resultTools.first { tool.name.removePrefix(basePrefix) == it.name.removePrefix(resultPrefix) }
 
             val recipe = SmithingRecipe(
-                NamespacedKey(this, equivalent.name),
+                NamespacedKey(this, tool.name + '_' + equivalent.name),
                 ItemStack(equivalent),
                 RecipeChoice.MaterialChoice(tool),
                 RecipeChoice.MaterialChoice(additionMaterial)
@@ -57,18 +61,50 @@ class BetterSmithing : JavaPlugin(), Listener {
         }
     }
 
-
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
+
+        val config = config
+        saveDefaultConfig()
+
+        val tiers = config.getConfigurationSection("tiers")
+        val tiersMap = hashMapOf(
+            "leather" to leatherTools,
+            "wooden" to woodenTools,
+            "stone" to stoneTools,
+            "iron" to ironTools,
+            "golden" to goldenTools,
+            "diamond" to diamondTools
+        )
+
+        for ((tier, tools) in tiersMap) {
+            if (tiers?.getConfigurationSection(tier) != null) {
+                val upgradeTo = tiers.getString("$tier.upgrade_to")
+                val upgradeItem = tiers.getString("$tier.upgrade_item")
+                if (upgradeTo != null) {
+                    val upgradeToTools = tiersMap[upgradeTo]
+                    if (upgradeToTools != null && upgradeItem != null) {
+                        addToolsRecipe(
+                            tools,
+                            upgradeToTools,
+                            Material.valueOf(upgradeItem.toUpperCase()),
+                            "${tier.toUpperCase()}_",
+                            "${upgradeTo.toUpperCase()}_"
+                        )
+                    }
+                }
+            }
+        }
+
 
         val pluginId = 17593 // <-- Replace with the id of your plugin!
 
         Metrics(this, pluginId)
-
-        addToolsRecipe(woodenTools, stoneTools, Material.STONE, "WOODEN_", "STONE_")
-        addToolsRecipe(stoneTools, ironTools, Material.IRON_INGOT, "STONE_", "IRON_")
-        addToolsRecipe(ironTools, goldenTools, Material.GOLD_INGOT, "IRON_", "GOLDEN_")
-        addToolsRecipe(goldenTools, diamondTools, Material.DIAMOND, "GOLDEN_", "DIAMOND_")
+//
+//        addToolsRecipe(woodenTools, stoneTools, Material.STONE, "WOODEN_", "STONE_")
+//        addToolsRecipe(stoneTools, ironTools, Material.IRON_INGOT, "STONE_", "IRON_")
+//        addToolsRecipe(ironTools, goldenTools, Material.GOLD_INGOT, "IRON_", "GOLDEN_")
+//        addToolsRecipe(goldenTools, diamondTools, Material.DIAMOND, "GOLDEN_", "DIAMOND_")
     }
 
     override fun onDisable() {
